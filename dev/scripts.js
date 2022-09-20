a820180f1eb28509b78e56bdeee8f0dc58586d72
Wireinet <wireinet@ya.ru>
2022-09-20 21:30:26 +0300

    new matrix

--- dev/scripts.js
+++ dev/scripts.js
@@ -2,5 +2,3 @@
 var body = document.body,
     html = document.documentElement;
 var bodyHeight = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-
-// $(function(){$(body).click(function(){alert();})});
